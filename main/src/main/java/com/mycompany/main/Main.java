175064be8466ceaea0d17c5f299085ed749893f2
Abinisha29 <118199306+Abinisha29@users.noreply.github.com>
2022-12-03 13:04:46 +0530

    shop

---
+++ main/src/main/java/com/mycompany/main/Main.java
@@ -0,0 +1,33 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Project/Maven2/JavaApp/src/main/java/${packagePath}/${mainClassName}.java to edit this template
+ */
+
+package com.mycompany.main;
+
+/**
+ *
+ * @author Abi
+ */
+public class Main {
+
+    public static void main(String[] args) {
+        shop joyra=new shop();
+        joyra.shop_name="joyra";
+        joyra.no_of_shops=5;
+        joyra.owner_name="Johnson";
+        joyra.no_of_things=2100;
+        
+        shop lyns=new shop();
+        lyns.shop_name="lyns";
+        lyns.owner_name="linny";
+        lyns.no_of_things=30000;
+        lyns.no_of_shops=21;
+             
+        
+    }
+    {
+        
+    }
+    }
+}
---
+++ main/src/main/java/com/mycompany/main/shop.java
@@ -0,0 +1,23 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package com.mycompany.main;
+
+/**
+ *
+ * @author Abi
+ */
+public class shop {
+    String shop_name;
+    String owner_name;
+    float no_of_shops;
+    float no_of_things;
+    void display_shop_name()
+    {
+        System.out.println("The of shop is "+shop_name);
+
+        
+    }
+    
+}

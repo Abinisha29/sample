62f5ff4a0f054dfca93867409386a0fb3ec13254
Abinisha29 <118199306+Abinisha29@users.noreply.github.com>
2022-12-03 09:46:48 +0530

    pen

---
+++ pen/src/main/java/com/mycompany/pen/Pen.java
@@ -0,0 +1,39 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Project/Maven2/JavaApp/src/main/java/${packagePath}/${mainClassName}.java to edit this template
+ */
+
+package com.mycompany.pen;
+
+/**
+ *
+ * @author Abi
+ */
+public class Pen {
+                
+
+    public static void main(String[] args) {
+        
+            types pen=new types();
+            pen.height=6;
+            pen.model="ball point";
+            pen.pen_name="stylish";
+            pen.width=1;
+            {
+                System.out.println("The name of pen name is "+pen.pen_name);
+            }
+            {
+                System.out.println("The width of pen is "+pen.width);
+            }
+            
+            
+            
+            
+            
+                
+                
+                
+                
+        
+    }
+}
---
+++ pen/src/main/java/com/mycompany/pen/types.java
@@ -0,0 +1,27 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package com.mycompany.pen;
+
+/**
+ *
+ * @author Abi
+ */
+public class types {
+    String pen_name;
+        String model;
+        float height;
+        float width;
+         void width(){
+         System.out.println("the width of pen is "+width);
+         }
+         
+         void model_name(){
+         System.out.println("the model of pen is "+model);
+         }
+         void height(){
+         System.out.println("the heightof pen is "+height );
+         }
+    
+}
